e48d34a8cc7be106a437bb18f6126f83fa8545bc
Jordi-Llorens <jordillorensterrazas@gmail.com>
2021-08-19 18:20:05 +0200

    updated code

--- exercises.R
+++ exercises.R
@@ -1,9 +1,37 @@
-#
-# 1)
-# Change the function "hello" so
-# that instead of returning "Hello?"
-# it returns "Hello Data Science!"
+# 1. ----
+# Complete the code to return only even numbers
 
-hello <- function(){
-    return('Hello Data Science!')
+even <- function(vector){
+  out <- NULL
+  for (var in vector) {
+    if (var %% 2 != 0) {
+      # your code
+    }  
+    out <- c(out,var)
+  }
+  return(out)
 }
+
+
+# 2. ----
+# Complete the code so that is TRUE 
+# when the 1st element of bool_vec1 is TRUE or 
+# the 1st element of bool_vec2 is TRUE
+
+is.first.true <- function(bool_vec1,bool_vec2){
+  # your code
+}
+
+
+# 3. ----
+# Complete the code to return a vector ranging from n to zero
+
+countdown <- function(n){
+  # your code
+  while(n >= 0) {
+    vec <- c(vec, n)
+    # your code
+  }
+  return(vec)
+}
+
--- test_exercises.R
+++ test_exercises.R
@@ -1,17 +1,48 @@
 source('exercises.R')
-
+# source('solutions.R')
 correct <- 0
 errors <- c()
 
-if (identical(hello(),"Hello Data Science!")){
+# 1. even ----
+
+if (!is.null(even(c(2,3,4))) &&
+  identical(even(c(2,3,4)),c(2,4))){
   correct <- correct + 1
 } else {
-  errors <- c(errors, 'hello() does not print Hello Data Science!')
+  errors <- c(errors, 'even does not filter out odd')
 }
 
-if (correct < 1){
+# 2. is.first.true ----
+
+bool_vec1 <- c(TRUE, FALSE, FALSE)
+bool_vec2 <- c(FALSE, TRUE, TRUE)
+if (!is.null(is.first.true(bool_vec1, bool_vec2)) &&
+            is.first.true(bool_vec1, bool_vec2)){
+  correct <- correct + 1
+} else {
+  errors <- c(errors, 
+              'bool_vec1[1] is TRUE but output of eq_at_least is not TRUE')
+}
+
+# 3. countdown ----
+
+n <- 10
+
+if (!is.null(countdown(n)) &&
+    identical(countdown(n), as.numeric(10:0))){
+  correct <- correct + 1
+} else {
+  errors <- c(errors, 
+              'countdown(10) != 10:0')
+}
+
+
+# Grading ----
+
+n <- 3
+if (correct < n){
   print(errors)
-  stop('Grade: ',round(correct/1,2)*100)
+  stop('Grade: ',round(correct/n,2)*100)
 } else {
   print(paste('Grade: ', 100))
 }
